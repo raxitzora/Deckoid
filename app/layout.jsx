0d9096dfa1f5f4e37fe33fa5b4f2c02b63125d20
raxitzora <raxitzora@gmail.com>
2025-07-21 00:17:12 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -41,8 +41,8 @@ export default function RootLayout({ children }) {
         imageAlt="Animated Turtle"
         reverse={true}
       />
-        
-       {children}
+      
+     {children}
         <Footer />
       </body>
     </html>
